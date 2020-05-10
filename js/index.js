202e5667c3137669bdad310807be5640c0f065e3
caparra92 <caparra92@gmail.com>
2020-05-10 20:03:41 -0300

    Adicionando estilos about y margenes

--- js/index.js
+++ js/index.js
@@ -38,42 +38,62 @@ let btnBusca = document.getElementById('btn_busca');
 
 btnBusca.addEventListener('click', function (e) {
 
+    let inputPlace = document.getElementById('place');
     let place = document.getElementById('place').value;
-    let checkIn = document.getElementById('check-in').value;
-    let checkOut = document.getElementById('check-out').value;
-    let dateIn = new Date(checkIn).getTime();
-    let dateOut = new Date(checkOut).getTime();
-    let seconds = dateOut - dateIn;
-    let days = seconds / (1000*60*60*24);
-    let container = document.getElementById('cardContainer');
+    let checkIn = document.getElementById('check-in');
+    let checkOut = document.getElementById('check-out');
+    let mapid = document.getElementById('mapid');
 
-    if (place === '') {
-        alert('Tenta escrever algum lugar pra procurar');
+    if (checkIn.value > checkOut.value) {
+        alert('A data de checkout deve ser maior a data de checkin');
+        e.preventDefault();
+        e.stopPropagation();
+        checkIn.classList.add('danger');
+        checkOut.classList.add('danger');
     } else {
+        let dateIn = new Date(checkIn.value).getTime();
+        let dateOut = new Date(checkOut.value).getTime();
+        let seconds = dateOut - dateIn;
+        let days = seconds / (1000 * 60 * 60 * 24);
+        let container = document.getElementById('cardContainer');
 
-        window.location = 'index.html#mapContainer';
-        //mapa
-        //
-        //console.log(place);
-        const matchedArray = findMatches(place, cities);
-        container.innerHTML = '';
+        if (place === '' || checkIn.value === '' || checkOut.value === '') {
+            e.preventDefault();
+            e.stopPropagation();
+            inputPlace.classList.add('danger');
+            checkIn.classList.add('danger');
+            checkOut.classList.add('danger');
+        } else {
+            inputPlace.classList.remove('danger');
+            checkIn.classList.remove('danger');
+            checkOut.classList.remove('danger');
+            mapid.style.height = '500px';
+            mapid.style.width = '100%';
+            mapid.style.margin = '0 auto';
+            window.location = 'index.html#mapContainer';
 
-        var mymap = L.map('mapid').setView([matchedArray[0].latitude, matchedArray[0].longitude], 12);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FwYXJyYTkyIiwiYSI6ImNrOXcxMmozOTAyMW8zZnFzcGNmMm8wOWUifQ.W9lU97CRGtGpCtdCdTw3mw', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 17,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoiY2FwYXJyYTkyIiwiYSI6ImNrOXcxMmozOTAyMW8zZnFzcGNmMm8wOWUifQ.W9lU97CRGtGpCtdCdTw3mw'
-        }).addTo(mymap);
+            const matchedArray = findMatches(place, cities);
+            container.innerHTML = '';
+            container.innerHTML = `<div class="col-md-12">
+                                <h1 class="p-5">Temos algumas boas opções pra vc</h1>
+                                </div>`
 
-        for (let item of matchedArray) {
+            var mymap = L.map('mapid').setView([matchedArray[0].latitude, matchedArray[0].longitude], 12);
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FwYXJyYTkyIiwiYSI6ImNrOXcxMmozOTAyMW8zZnFzcGNmMm8wOWUifQ.W9lU97CRGtGpCtdCdTw3mw', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 17,
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: 'pk.eyJ1IjoiY2FwYXJyYTkyIiwiYSI6ImNrOXcxMmozOTAyMW8zZnFzcGNmMm8wOWUifQ.W9lU97CRGtGpCtdCdTw3mw'
+            }).addTo(mymap);
 
-            console.log(item.latitude,item.longitude);
-            var marker = L.marker([item.latitude, item.longitude]).addTo(mymap);
-            container.innerHTML += `<div class="col-md-3">
-                <div class="card">
+            for (let item of matchedArray) {
+
+                console.log(item.latitude, item.longitude);
+                var marker = L.marker([item.latitude, item.longitude]).addTo(mymap);
+                container.innerHTML += `<div class="col-md-3">
+                <div class="card info">
                     <img src="${item.photo}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${item.propertyType}</h5>
@@ -86,7 +106,9 @@ btnBusca.addEventListener('click', function (e) {
                     </div>
                 </div>
                 </div>`
+            }
         }
     }
+
 
 });
